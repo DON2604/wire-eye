3203517c82a7575dddfd5d26d0ce25a1ff22f0fa
techmonk000 <swarnavomukherjee03@gmail.com>
2023-09-17 13:12:13 +0530

    added firebase

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FirebaseProvider } from './context/Firebase';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
   </React.StrictMode>
 );

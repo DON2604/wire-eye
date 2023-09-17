3c7465145a7dc6dd940ff21f3bc69fed33aba302
DON2604 <mrinmaydas2604@gmail.com>
2023-09-17 13:10:52 +0530

    ui updates

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (<>
     <Header />
     <div className="App">
-      hello
+      
     </div>
     </>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,17 @@
+import '../index.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import logoImage from '../Assets/eye.png';
 
 function Header() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme ="dark" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Wire-Eye</Navbar.Brand>
+        <Navbar.Brand href="#home" className="brand">
+        <img src= {logoImage} alt="Logo" className="logo small " />
+          Wire-Eye
+          </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">

48e3220c9007b2443da93de8c1a0c5d78f01d1e2
Mrinmay Das <mrinmaydas2604@gmail.com>
2023-09-19 02:01:56 +0530

    thats what she said

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,34 +1,51 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import logoImage from '../Assets/eye.png';
-import '../index.css'; 
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import logoImage from "../Assets/eye.png";
+import "../index.css";
 
 function Header() {
   return (
-      <nav className="navbar navbar-expand-lg navbar-custom">
-          <a className="navbar-brand" href="#">
-              <img src={logoImage} alt="Logo" className="logo" />
-              <span className="website-name">Website Name</span>
-          </a>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
-              <ul className="navbar-nav">
-                  <li className="nav-item">
-                      <a className="nav-link" href="#">Home</a>
-                  </li>
-                  <li className="nav-item">
-                      <a className="nav-link" href="#">Status</a>
-                  </li>
-                  <li className="nav-item">
-                      <a className="nav-link" href="#">Problem</a>
-                  </li>
-              </ul>
-          </div>
-      </nav>
+    <nav className="navbar navbar-expand-lg navbar-custom">
+      <a className="navbar-brand" href="#">
+        <img src={logoImage} alt="Logo" className="logo" />
+        <span className="website-name">Website Name</span>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div
+        className="collapse navbar-collapse justify-content-end"
+        id="navbarNav"
+      >
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Status
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Problem
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 }
 
--- src/Components/status.js
+++ src/Components/status.js
@@ -4,46 +4,51 @@ import "../Styles/status.css";
 const status = () => {
   return (
     <>
-    <div className="home-container"> <br/>   
-    <h1 className="sthead">Status Report</h1>
-    <div class="frame">
-  <div class="center">
-    
-		<div class="profile">
-			<div class="image">
-				<div class="circle-1"></div>
-				<div class="circle-2"></div>
-				<img src="https://100dayscss.com/codepen/jessica-potter.jpg" width="70" height="70" alt="Jessica Potter"/>
-			</div>
-			
-			<div class="name">Jessica Potter</div>
-			<div class="job">Visual Artist</div>
-			
-			<div class="actions">
-				<button class="btn">Follow</button>
-				<button class="btn">Message</button>
-			</div>
-		</div>
-		
-		<div class="stats">
-			<div class="box">
-				<span class="value">523</span>
-				<span class="parameter">Posts</span>
-			</div>
-			<div class="box">
-				<span class="value">1387</span>
-				<span class="parameter">Likes</span>
-			</div>
-			<div class="box">
-				<span class="value">146</span>
-				<span class="parameter">Follower</span>
-			</div>
-		</div>
-  </div>
-</div>
-    </div>
+      <div className="home-container">
+        <br />
+        <h1 className="sthead">Status Report</h1>
+        <div className="frame">
+          <div className="center">
+            <div className="profile">
+              <div className="image">
+                <div className="circle-1"></div>
+                <div className="circle-2"></div>
+                <img
+                  src="https://100dayscss.com/codepen/jessica-potter.jpg"
+                  width="70"
+                  height="70"
+                  alt="Jessica Potter"
+                />
+              </div>
+
+              <div className="name">Jessica Potter</div>
+              <div className="job">Visual Artist</div>
+
+              <div className="actions">
+                <button className="btn">Follow</button>
+                <button className="btn">Message</button>
+              </div>
+            </div>
+
+            <div className="stats">
+              <div className="box">
+                <span className="value">523</span>
+                <span className="parameter">Posts</span>
+              </div>
+              <div className="box">
+                <span className="value">1387</span>
+                <span className="parameter">Likes</span>
+              </div>
+              <div className="box">
+                <span className="value">146</span>
+                <span className="parameter">Follower</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
 export default status;

f7f5f476b5e7420e50e3ccb27597c2fa89f52b86
Mrinmay Das <mrinmaydas2604@gmail.com>
2023-09-19 00:20:03 +0530

    ui big updates

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,29 +7,28 @@ import '../index.css';
 
 function Header() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" data-bs-theme="dark" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home" className="brand">
-          <img src={logoImage} alt="Logo" className="logo small" />
-          Wire-Eye
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto"></Nav>
-          <Nav className="ml-auto"> {/* Use ml-auto to move items to the right */}
-            <Nav.Link eventKey={1} href="#" className='mx-3'>
-              Home
-            </Nav.Link>
-            <Nav.Link eventKey={2} href="#" className='mx-3'>
-              Problems
-            </Nav.Link>
-            <Nav.Link eventKey={3} href="#" className='mx-0'>
-              Status
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      <nav className="navbar navbar-expand-lg navbar-custom">
+          <a className="navbar-brand" href="#">
+              <img src={logoImage} alt="Logo" className="logo" />
+              <span className="website-name">Website Name</span>
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+              <ul className="navbar-nav">
+                  <li className="nav-item">
+                      <a className="nav-link" href="#">Home</a>
+                  </li>
+                  <li className="nav-item">
+                      <a className="nav-link" href="#">Status</a>
+                  </li>
+                  <li className="nav-item">
+                      <a className="nav-link" href="#">Problem</a>
+                  </li>
+              </ul>
+          </div>
+      </nav>
   );
 }
 

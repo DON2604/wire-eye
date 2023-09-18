30869b54a15a57d415301038449fcf02f545ffc6
Raj Bhattacharyya <rajbhattacharyya18110@gmail.com>
2023-09-18 20:46:03 +0530

    added realtime database

--- src/context/Firebase.js
+++ src/context/Firebase.js
@@ -1,28 +1,36 @@
-import { createContext } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCXzG71LawqujLj3uwxor06w3xbZQWphns",
   authDomain: "wire-eye.firebaseapp.com",
-  databaseURL: "https://wire-eye-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL:
+    "https://wire-eye-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "wire-eye",
   storageBucket: "wire-eye.appspot.com",
   messagingSenderId: "336759982231",
   appId: "1:336759982231:web:c9ad28d3daa8ad4c6741d2",
-  measurementId: "G-R8YK20YEDK"
+  measurementId: "G-R8YK20YEDK",
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getDatabase(firebaseApp);
 const FirebaseContext = createContext(null);
 
-export const FirebaseProvider = (props) => {
-    
+export const useFirebase = () => useContext(FirebaseContext);
 
-    return (
-        <FirebaseContext.Provider>
-            {props.children}
-        </FirebaseContext.Provider>
-    )
-}
+export const FirebaseProvider = (props) => {
+  const [Post, setPost] = useState("");
+
+  useEffect(() => {
+    onValue(ref(database, "Post1"), (snapshot) => setPost(snapshot.val()));
+  }, []);
+
+  return (
+    <FirebaseContext.Provider value={{}}>
+      {/* <h3>{Post}</h3> */}
+      {props.children}
+    </FirebaseContext.Provider>
+  );
+};

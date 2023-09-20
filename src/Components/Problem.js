d61cca2b9054d5016e85e814f517998fd2a241ed
DON2604 <mrinmaydas2604@gmail.com>
2023-09-20 21:36:36 +0530

    ui fixes final

--- src/Components/Problem.js
+++ src/Components/Problem.js
@@ -1,15 +1,28 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import "../Styles/problem.css"
 
 const Problem = () => {
   const location = useLocation();
   const problem = location.state?.problem;
 
   return (
-    <div>
-      <h2>{problem && <p>Problem: {problem} is not Working</p>}</h2>
+    <>
+    <h2 className="box-heading">Problem Identified 
+    </h2>
+    <div className="problem-box">     
+      <div className="message-box">
+        <p className="message">Problem Identified at
+        <div className="text">
+        <strong>{problem}</strong>
+        </div>
+        Possible cause can be faulty street light.<br/></p>
+      </div>
     </div>
-  );
+    <div className="ut"></div>
+    </>
+    );
+
 };
 
 export default Problem;
